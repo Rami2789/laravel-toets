cd7c60a124e0eb5ad83060ee5c4a18493b9e431e
Rami <74742680+Rami2789@users.noreply.github.com>
2023-01-22 18:38:25 +0100

    Route aangemaakt voor de controller en als je inlogd brengt het systeem je gelijk naar de test pagina

--- laravel-toets/routes/web.php
+++ laravel-toets/routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\TestController; 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +26,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('/test', TestController::class)->middleware(['auth']);
 });
 
 require __DIR__.'/auth.php';
